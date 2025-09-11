39cc909e931dff4a1b91b4103af57cc1dc9aaacb
whiteNight39 <gboladefred@gmail.com>
2025-09-11 17:37:45 +0100

    Minor fixes to config files

--- src/main/java/com/studyhive/config/OpenAPIConfig.java
+++ src/main/java/com/studyhive/config/OpenAPIConfig.java
@@ -2,8 +2,11 @@ package com.studyhive.config;
 
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
 
 @Configuration
 public class OpenAPIConfig {
@@ -14,6 +17,10 @@ public class OpenAPIConfig {
                 .info(new Info()
                         .title("Fredrick 🖤 | StudyHive API Documentation")
                         .description("Comprehensive documentation for StudyHive's backend.")
-                        .version("1.0.0"));
+                        .version("1.0.0"))
+                .servers(List.of(
+                        new Server().url("http://localhost:8080").description("Local Dev"),
+                        new Server().url("https://studyhive-5iel.onrender.com").description("Render Deployment")
+                ));
     }
 }
